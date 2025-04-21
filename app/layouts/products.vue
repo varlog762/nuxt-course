25e4ac0ae263496383f22a4865331e3709b961a5
Gregory Pereverzev <boot762@gmail.com>
2025-04-21 16:30:17 +0300

    update products layout

--- app/layouts/products.vue
+++ app/layouts/products.vue
@@ -4,14 +4,19 @@
   <div>
     <header>
       <nav>
-        <ul>
-          <li><NuxtLink to="/">Home</NuxtLink></li>
-        </ul>
+        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
       </nav>
     </header>
     <main>
       <slot />
     </main>
+    <footer>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </ul>
+    </footer>
   </div>
 </template>
 
