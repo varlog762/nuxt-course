76cf0f2d120d3c262c603ef1c00017b1fc68d6e2
Gregory Pereverzev <boot762@gmail.com>
2025-04-22 15:53:32 +0300

    feat: update Products page

--- app/pages/products/index.vue
+++ app/pages/products/index.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
+import type { ProductInterface } from '~/types/product.interface';
+
 definePageMeta({
   layout: 'products',
 });
+
+const { data: products } = await useFetch<ProductInterface[]>(
+  `${import.meta.env.VITE_API_URL}/products`
+);
 </script>
 
 <template>
   <div>
-    <h2>Products</h2>
+    <div v-if="products" class="grid grid-cols-4 gap-5">
+      <div v-for="product in products" :key="product.id">
+        {{ product.id }}
+      </div>
+    </div>
   </div>
 </template>
 
