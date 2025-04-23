9092b30e9d936a238d80288da5bcc7a08f21bdab
Gregory Pereverzev <boot762@gmail.com>
2025-04-23 20:21:42 +0300

    feat: add ProductDetails component

--- app/pages/products/[id].vue
+++ app/pages/products/[id].vue
@@ -11,16 +11,8 @@ const { data: product } = await useFetch<ProductInterface>(uri);
 </script>
 
 <template>
-  <div>
-    <h2>Product {{ id }}</h2>
-    <div>
-      <h3>{{ product?.title }}</h3>
-      <p>{{ product?.price }}</p>
-      <div>
-        <img :src="product?.image" alt="Product photo" />
-      </div>
-      <div>{{ product?.description }}</div>
-    </div>
+  <div v-if="product">
+    <ProductDetails :product />
   </div>
 </template>
 
