52098c264a02a3276a5f89c0518a4d41bfbdb448
Gregory Pereverzev <boot762@gmail.com>
2025-04-23 20:02:40 +0300

    feat: add ProductCard component

---
+++ app/components/ProductCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { ProductInterface } from '~/types/product.interface';
+
+defineProps<{
+  product: ProductInterface;
+}>();
+</script>
+
+<template>
+  <div class="card text-center">
+    <img :src="product.image" alt="Product photo" class="thumb" />
+    <p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
+    <NuxtLink :to="`/products/${product.id}`">
+      <p class="btn my-4">View Details</p>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.thumb {
+  max-height: 120px;
+  max-width: 70%;
+  margin: 0 auto;
+}
+</style>
