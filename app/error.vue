7860ee01f6619e55a72aa042c34c319fc1e70185
Gregory Pereverzev <boot762@gmail.com>
2025-04-24 20:19:50 +0300

    feat: add error page

---
+++ app/error.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  error: {
+    statusCode: string;
+    message: string;
+  };
+}>();
+</script>
+
+<template>
+  <div class="mt-7 max-w-sm mx-auto text-center card">
+    <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
+    <p class="mt-7 text-6xl">Ooops...</p>
+    <p class="mt-7">{{ error.message }}</p>
+  </div>
+</template>
+
+<style scoped></style>
--- app/pages/products/[id].vue
+++ app/pages/products/[id].vue
@@ -8,6 +8,13 @@ const { id } = useRoute().params;
 const uri = `${import.meta.env.VITE_API_URL}/products/${id}`;
 
 const { data: product } = await useFetch<ProductInterface>(uri);
+
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Product not found!',
+  });
+}
 </script>
 
 <template>
